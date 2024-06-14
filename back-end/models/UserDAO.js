e60dd1a643c9a01adb309100fbfc0be1e24d4e3c
Benjamin CHAILLAN <benjamin.chaillan-pro@laposte.net>
2024-06-14 16:01:09 +0200

    auth and register back-end done

--- back-end/models/UserDAO.js
+++ back-end/models/UserDAO.js
@@ -5,6 +5,39 @@ class UserDAO extends AbstractDAO {
     super(db);
     this.table = 'player';
   }
+  create(username, email, password) {
+    return new Promise((resolve, reject) => {
+      console.log(username, email, password);
+      this.connection.execute(
+        `INSERT INTO ${this.table} (username, email, password) VALUES (?,?,?)`,
+        [username, email, password],
+        (err, result, fields) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(result);
+        }
+      );
+    });
+  }
+
+  findByMail(email) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(
+        `SELECT id, username, email, password
+        FROM ${this.table} WHERE email = ? 
+          `,
+        [email],
+        (err, result, field) => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve(result);
+        }
+      );
+    });
+  }
 }
 
 export default UserDAO;

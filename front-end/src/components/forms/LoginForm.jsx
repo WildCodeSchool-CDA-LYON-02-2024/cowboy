62bf32ad9f59fb1c3564d9f8a1148c9e59848832
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2024-06-17 10:25:07 +0200

    init dep in front and finish register login form

---
+++ front-end/src/components/forms/LoginForm.jsx
@@ -0,0 +1,195 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleMailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  return (
+    <Box
+      component="form"
+      // onSubmit={handleSubmit}
+      sx={{
+        border: "2px black",
+        height: "30rem",
+        width: "20rem",
+        paddingTop: "3rem",
+      }}
+    >
+      <Paper
+        sx={{
+          width: "100%",
+          height: "100%",
+          borderRadius: "1rem",
+          backgroundColor: "#565656B3",
+          "&.MuiBox-root ": {
+            fontFamily: "Pixelify",
+            fontWeight: 400,
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              pt: "2rem",
+              gap: "2rem",
+              width: "100%",
+            }}
+          >
+            <Typography
+              variant="h4"
+              className=" text-center text-white"
+              sx={{
+                pt: "1rem",
+                fontFamily: "Pixelify",
+                textShadow:
+                  "1px 1px 0px black, -1px 1px 0px black, 1px -1px 0px black, -1px -1px 0px black",
+              }}
+            >
+              BON RETOUR COWBOY
+            </Typography>
+            <FormControl
+              sx={{
+                width: "80%",
+                mt: "0.5rem",
+                fontFamily: "Pixelify",
+                textShadow:
+                  "1px 1px 0px black, -1px 1px 0px black, 1px -1px 0px black, -1px -1px 0px black",
+              }}
+            >
+              <InputLabel
+                sx={{
+                  color: "white",
+                  fontFamily: "Pixelify",
+                  "&.Mui-focused": {
+                    color: "white",
+                  },
+                }}
+              >
+                Email
+              </InputLabel>
+              <OutlinedInput
+                onChange={handleMailChange}
+                value={email}
+                label="Email"
+                sx={{
+                  height: "3rem",
+                  "& input": {
+                    color: "white",
+                    fontFamily: "Pixelify",
+                  },
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "white",
+                  },
+                  "&:hover .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "white",
+                  },
+                  "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "white",
+                  },
+                }}
+              />
+            </FormControl>
+            <FormControl
+              sx={{
+                width: "80%",
+                fontFamily: "Pixelify",
+                textShadow:
+                  "1px 1px 0px black, -1px 1px 0px black, 1px -1px 0px black, -1px -1px 0px black",
+              }}
+            >
+              <InputLabel
+                sx={{
+                  color: "white",
+                  fontFamily: "Pixelify",
+                  "&.Mui-focused": {
+                    color: "white",
+                  },
+                }}
+              >
+                Mot de passe
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                onChange={handlePasswordChange}
+                type="password"
+                value={password}
+                label="Mot de passe"
+                sx={{
+                  height: "3rem",
+                  "& input": {
+                    color: "white",
+                    fontFamily: "Pixelify",
+                  },
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "white",
+                  },
+                  "&:hover .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "white",
+                  },
+                  "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "white",
+                  },
+                }}
+              />
+            </FormControl>
+          </Box>
+          <Typography
+            sx={{
+              fontSize: "0.7rem",
+              pt: "1rem",
+              fontFamily: "Pixelify",
+              color: "white",
+              textShadow:
+                "1px 1px 0px black, -1px 1px 0px black, 1px -1px 0px black, -1px -1px 0px black",
+            }}
+          >
+            Pas encore de compte? Inscrivez vous !
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{
+              mt: "2rem",
+              width: "60%",
+              backgroundColor: "#1D1C1C",
+              "&:hover": {
+                backgroundColor: "#333333",
+              },
+              fontFamily: "Pixelify",
+              textShadow:
+                "1px 1px 0px black, -1px 1px 0px black, 1px -1px 0px black, -1px -1px 0px black",
+            }}
+            type="submit"
+          >
+            Se connecter
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
+  );
+}
---
+++ front-end/src/pages/LoginPage.jsx
@@ -0,0 +1,44 @@
+import { Box, Container, keyframes } from "@mui/material";
+import logo from "../assets/images/LOGO.png";
+import backgroundImage from "../assets/images/cactus.jpg";
+import LoginForm from "../components/forms/LoginForm";
+
+export default function LoginPage() {
+  const scrollBackground = keyframes`
+  0% { background-position: 0 0; }
+  100% { background-position: -100vw 0; }
+`;
+
+  return (
+    <Container
+      maxWidth="false"
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        margin: 0,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundImage: `url(${backgroundImage}),url(${backgroundImage})`,
+        backgroundSize: "auto 100%",
+        backgroundRepeat: "repeat-x",
+        animation: `${scrollBackground} 30s linear infinite`,
+        "@media (min-width: 600px)": {
+          animation: `${scrollBackground} 45s linear infinite`,
+          backgroundSize: "100vw 100vh",
+        },
+        "@media (min-width: 1200px)": {
+          animation: `${scrollBackground} 30s linear infinite`,
+          backgroundSize: "100vw 100vh",
+        },
+      }}
+    >
+      <Box
+        component="img"
+        src={logo}
+        sx={{ height: "10rem", width: "12rem", mt: "2rem" }}
+      />
+      <LoginForm />
+    </Container>
+  );
+}

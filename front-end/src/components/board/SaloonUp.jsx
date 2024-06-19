0c6de8aa86a36dd1d5d3f15f80fb3c1cf6ef3a88
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2024-06-19 10:56:30 +0200

    save progress

---
+++ front-end/src/components/board/SaloonUp.jsx
@@ -0,0 +1,11 @@
+import { Box, Container, Typography } from "@mui/material";
+
+export default function SaloonUp() {
+  return (
+    <Container>
+      <Box>
+        <Typography>lvl:</Typography>
+      </Box>
+    </Container>
+  );
+}

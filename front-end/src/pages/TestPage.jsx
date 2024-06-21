5dfd877b986079c4e12b2d3e43b62e46e4e330bf
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2024-06-21 10:46:20 +0200

    update test page

--- front-end/src/pages/TestPage.jsx
+++ front-end/src/pages/TestPage.jsx
@@ -1,23 +1,40 @@
-// import { Button } from "@mui/material";
-// import { useNavigate } from "react-router-dom";
-// import { usePlayerContext } from "../context/PlayerContext";
+import { Box, Container, Typography } from "@mui/material";
+import Barillet from "../assets/images/barillet-sbg.png";
 
 export default function TestPage() {
-  // const { logout } = usePlayerContext();
-
-  // const navigate = useNavigate();
-  // const logoutFromSession = () => {
-  //   logout();
-  //   navigate("/");
-  //   console.info("DISCONNECTED");
-  // };
-
   return (
-    <div>
-      TestPage
-      {/* <Button variant="contained" onClick={logoutFromSession}>
-        Déconnexion
-      </Button> */}
-    </div>
+    <Container
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "black",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+        textShadow:
+          "1px 1px 0px black, -1px 1px 0px black, 1px -1px 0px black, -1px -1px 0px black",
+      }}
+    >
+      <Typography variant="h3" sx={{ fontFamily: "Pixelify", pb: "1.5rem" }}>
+        LOADING
+      </Typography>
+      <Box
+        component="img"
+        src={Barillet}
+        sx={{
+          animation: "spin 1.5s infinite",
+          "@keyframes spin": {
+            "0%": {
+              transform: "rotate(0deg)",
+            },
+            "100%": {
+              transform: "rotate(360deg)",
+            },
+          },
+        }}
+      />
+    </Container>
   );
 }

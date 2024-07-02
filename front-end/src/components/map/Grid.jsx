349b3ebe126268aff7aafe6a0db172ef42a3ff34
Benjamin Chaillan <benjamin.chaillan-pro@laposte.net>
2024-07-02 13:54:39 +0200

    modal init with ressource, and close modal

--- front-end/src/components/map/Grid.jsx
+++ front-end/src/components/map/Grid.jsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material';
 import Cell from './cell/Cell.jsx';
 import Bois from '../../assets/images/ressources/buche-bois.png';
 import Gold from '../../assets/images/ressources/pepite-or.png';
@@ -27,6 +28,10 @@ const Grid = ({ rows, cols }) => {
     setSlotId(slots.find((slot) => slot.id === id));
     fetchResourceOnSlot(id, setResourceSlot);
     setModal(true);
+  };
+
+  const handleClose = () => {
+    setModal(false);
   };
 
   // Génération de la grille
@@ -78,7 +83,86 @@ const Grid = ({ rows, cols }) => {
               </div>
             </div>
             <div className='btn-container'>
-              <button className='collect-btn'>Collect</button>
+              <Button
+                variant='contained'
+                sx={{
+                  width: '60%',
+                  backgroundColor: '#1D1C1C',
+                  '&:hover': {
+                    backgroundColor: '#333333',
+                  },
+                  fontFamily: 'Pixelify',
+                  textShadow:
+                    '1px 1px 0px black, -1px 1px 0px black, 1px -1px 0px black, -1px -1px 0px black',
+                }}
+              >
+                Collecter
+              </Button>
+            </div>
+            <div className='colony-container'>
+              <h2 className='h2-colony'>Colonie</h2>
+            </div>
+            <div className='needed-ressources-container'>
+              <h2 className='h2-needed-ressources'>Ressource necessaire </h2>
+              <div className='resource-list-container'>
+                {/*TODO :  Changer le nommbre de ressources avec les ressources necessaire  */}
+                <div className='resource-li'>
+                  <img src={Gold} alt='Lingot or' style={{ height: '1rem' }} />
+                  <li> {resourceSlot[0].quantity} k </li>
+                </div>
+                <div className='resource-li'>
+                  <img src={Iron} alt='Metal' style={{ height: '1rem' }} />
+                  <li> {resourceSlot[1].quantity} k</li>
+                </div>
+                <div className='resource-li'>
+                  <img src={Stone} alt='Pierre' style={{ height: '1rem' }} />
+                  <li>{resourceSlot[2].quantity} k</li>
+                </div>
+                <div className='resource-li'>
+                  <img
+                    src={Bois}
+                    alt='Buche de bois'
+                    style={{ height: '1rem' }}
+                  />
+                  <li> {resourceSlot[3].quantity} k </li>
+                </div>
+              </div>
+            </div>
+            <div className='btn-container'>
+              <Button
+                variant='contained'
+                sx={{
+                  width: '60%',
+                  backgroundColor: '#1D1C1C',
+                  '&:hover': {
+                    backgroundColor: '#333333',
+                  },
+                  fontFamily: 'Pixelify',
+                  textShadow:
+                    '1px 1px 0px black, -1px 1px 0px black, 1px -1px 0px black, -1px -1px 0px black',
+                }}
+              >
+                Construire
+              </Button>
+            </div>
+            <div className='btn-container'>
+              <Button
+                onClick={handleClose}
+                variant='contained'
+                sx={{
+                  width: '50%',
+                  height: '15px',
+                  backgroundColor: '#1D1C1C',
+                  '&:hover': {
+                    backgroundColor: '#333333',
+                  },
+                  fontFamily: 'Pixelify',
+                  textShadow:
+                    '1px 1px 0px black, -1px 1px 0px black, 1px -1px 0px black, -1px -1px 0px black',
+                }}
+              >
+                Fermer
+              </Button>
             </div>
           </div>
         )}

411cfa7b3cca101ab9813b6ff4c939e70b5c8b77
Benjamin CHAILLAN <benjamin.chaillan-pro@laposte.net>
2024-07-12 12:03:56 +0200

    resolve bug for hiring cowboy when we have multiiple colony

--- back-end/models/CowboyDAO.js
+++ back-end/models/CowboyDAO.js
@@ -1,9 +1,9 @@
-import AbstractDAO from "./AbstractDAO.js";
+import AbstractDAO from './AbstractDAO.js';
 
 class CowboyModel extends AbstractDAO {
   constructor() {
     super();
-    this.table = "cowboy";
+    this.table = 'cowboy';
   }
 
   readAllDispo() {
@@ -11,7 +11,7 @@ class CowboyModel extends AbstractDAO {
       const query = `SELECT * FROM ${this.table} WHERE colony_id IS NULL`;
       this.connection.execute(query, (error, result) => {
         if (error) {
-          console.log("erreur model");
+          console.log('erreur model');
           return reject(error);
         } else {
           resolve(result);
@@ -37,13 +37,20 @@ class CowboyModel extends AbstractDAO {
   /* cow boy get hired */
 
   hiringCowboy(player_id, id) {
+    console.log('player_ID :', player_id);
+    console.log('ID  :', id);
     return new Promise((resolve, reject) => {
-      const query = `UPDATE ${this.table} SET colony_id = ( SELECT colony.id
-      FROM colony
-      JOIN map ON colony.map_id = map.id
-      JOIN player ON map.player_id = player.id
-      WHERE player.id = ?
-     ) WHERE id = ?`;
+      const query = `UPDATE ${this.table} 
+    SET colony_id = (
+    SELECT colony.id
+    FROM colony
+    JOIN map ON colony.map_id = map.id
+    JOIN player ON map.player_id = player.id
+    WHERE player.id = ?
+    ORDER BY colony.id ASC  
+    LIMIT 1                 
+    ) 
+    WHERE id = ?`;
       const values = [player_id, id];
 
       this.connection.execute(query, values, (error, result) => {

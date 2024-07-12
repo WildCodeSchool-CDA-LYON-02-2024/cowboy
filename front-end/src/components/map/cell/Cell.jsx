3a4664c963a7e3443c41af0084a9f6934fc8a450
Benjamin CHAILLAN <benjamin.chaillan-pro@laposte.net>
2024-07-12 13:54:32 +0200

    m

--- front-end/src/components/map/cell/Cell.jsx
+++ front-end/src/components/map/cell/Cell.jsx
@@ -3,13 +3,30 @@
 
 import home from '../../../assets/images/navbar/home.png';
 
-const Cell = ({ id, onClick, playerSlot }) => {
+const Cell = ({ id, onClick, playerSlot, otherSlot }) => {
   // Vérifier si playerSlot existe et si l'id actuel existe dans playerSlot
   const isPlayerSlot = playerSlot && playerSlot.includes(id);
 
+  // Vérifier si otherSlot existe et si l'id actuel existe dans otherSlot
+  const isOtherSlot = otherSlot && otherSlot.includes(id);
+
   return (
     <div className='cell' onClick={() => onClick(id)}>
-      {isPlayerSlot && <img src={home} alt='' className='img-home' />}
+      {isPlayerSlot && <img src={home} alt='' className='img-home' />}{' '}
+      {/* Slot du joueur actuel */}
+      {isOtherSlot && (
+        <img
+          src={home}
+          alt=''
+          className='img-home'
+          style={{
+            opacity: '0.5',
+            backgroundColor: 'red',
+            borderRadius: '10px',
+          }}
+        />
+      )}
+      {/* Slot de l'autre joueur */}
     </div>
   );
 };
